e27caaea5c9656b2853365f3a3f7498b16b6488a
SebastianManriqueM <prof.sebastian.manrique@gmail.com>
2024-09-09 10:47:22 -0600

    Add FIltering function based on string vectors for state and structure_type. Access by  mapping dictionary

--- src/tl_parameters_calc.jl
+++ src/tl_parameters_calc.jl
@@ -60,6 +60,9 @@ function get_bordering_states( df_us_states::DataFrame, user_filter::TL_FILTERS,
     @error( "$(user_filter.state[index_checked]) was not found to obtain its bordering states. Check TL_FILTERS.state[index]"  )
 end
 
+function get_filter_with_neighboring_states!(user_filter::TL_FILTERS)
+end
+
 
 function get_voltage_filtered_tl_df( df::DataFrame, user_filter::TL_FILTERS )
     if !(check_voltage_availability( user_filter.voltage_kv ))
@@ -68,7 +71,24 @@ function get_voltage_filtered_tl_df( df::DataFrame, user_filter::TL_FILTERS )
     return filter( row -> row[:voltage_kv] == user_filter.voltage_kv, df)
 end
 
-function get_filtered_tl_df( df::DataFrame, user_filter::TL_FILTERS )
+
+function get_tl_df_single_filter( df::DataFrame, user_filter::TL_FILTERS, key_df_column::String="state" )
+    index_df = COL_INDEX_MAP_TL[ key_df_column ]
+    if occursin( strip( lowercase("state") ) , strip( lowercase(key_df_column) ) )
+        user_filter_str_v = user_filter.state
+    elseif occursin( strip( lowercase("structure_type") ) , strip( lowercase(key_df_column) ) )
+        user_filter_str_v = user_filter.structure_type
+    else
+        @error("Please review key_df_column argument passed to $(nameof(var"#self#")) function. You can set this argument as 'state' or 'structure_type'. ")
+    end
+    # Define the filtering function
+    # **This filter has a Bug for STATES since "Kansas" is a substring or "Arkansas"
+    filter_func( row ) = any( s -> occursin( strip(lowercase(s)), coalesce(strip(lowercase(row[index_df]))) ), user_filter_str_v )
+    return filter(filter_func, df)
+end
+
+
+function get_tl_df_all_filters( df::DataFrame, user_filter::TL_FILTERS )
     # Voltage level
     filt_df  =  get_voltage_filtered_tl_df( df, user_filter )
 
@@ -78,7 +98,7 @@ function get_filtered_tl_df( df::DataFrame, user_filter::TL_FILTERS )
     end
 
     #N circuits
-    if user_filter.n_circuits > 2
+    if user_filter.n_circuits > 2 || user_filter.n_circuits < 1
         error("The current data set include information of Tower Geometries carrying up to 2 circuits. Please reconsider your selection of $(user_filter.n_circuits) circuits.")
     end
 
@@ -90,7 +110,7 @@ function get_filtered_tl_df( df::DataFrame, user_filter::TL_FILTERS )
     filt_df = filt_df2
 
     #N ground wires
-    if user_filter.n_ground_wire > 2
+    if user_filter.n_ground_wire > 2 || user_filter.n_ground_wire < 1
         error("The current data set include information of Tower Geometries carrying up to 2 ground wires. Please reconsider your selection of $(user_filter.n_ground_wire) ground wires.")
     end
 
@@ -103,11 +123,7 @@ function get_filtered_tl_df( df::DataFrame, user_filter::TL_FILTERS )
 
     #State
     if !( user_filter.state[1] == "" )
-        # Define the filtering function
-        # **This filter has a Bug since "Kansas" is a substring or "Arkansas"
-        filter_func_state(r) = any(s -> occursin( strip(lowercase(s)), coalesce(strip(lowercase(r[4]))) ), user_filter.state)
-        filt_df2 = filter(filter_func_state, filt_df)
-
+        filt_df2 = get_tl_df_single_filter( filt_df, user_filter, "state" )
         if nrow( filt_df2 ) < 1
             @warn( "Currently there is no data that match all the selected criteria. The state and structure type filters were ignored." )
             return filt_df
@@ -115,14 +131,9 @@ function get_filtered_tl_df( df::DataFrame, user_filter::TL_FILTERS )
         filt_df = filt_df2
     end
     
-
     #Structure type
     if !( user_filter.structure_type[1] == "" )
-        #filt_df2   = filter(row -> row[:structure_type] == user_filter.structure_type, filt_df)
-        # Define the filtering function
-        filter_func(row) = any(s -> occursin( strip(lowercase(s)), coalesce(strip(lowercase(row[2]))) ), user_filter.structure_type)
-        # Apply the filter to the DataFrame
-        filt_df2 = filter(filter_func, filt_df)
+        filt_df2 = get_tl_df_single_filter( filt_df, user_filter, "structure_type" )
         if nrow(filt_df2) < 1
             @warn( "Currently there is no data that match all the selected criteria. The structure type filter was ignored." )
             return filt_df
@@ -137,20 +148,22 @@ end
 
 
 #Read XLSX file with typical US tower Geometries
-file_rel_path     = "src/data/Tower_geometries_DB.xlsx"#src\data\Tower_geometries_DB.xlsx
+file_rel_path     = "src/data/Tower_geometries_DB.xlsx"
 sheet_tl_geometry = "TL_Geometry"
 sheet_us_states   = "Neighboring"
 df_tl_geometry    = DataFrame( XLSX.readtable(file_rel_path, sheet_tl_geometry) )
 df_us_states_info = DataFrame( XLSX.readtable(file_rel_path, sheet_us_states) )
 tl1_filter        = TL_FILTERS( 345, 2, 2, ["Indiana"], ["Pole"] )
 
-filt_df           = get_filtered_tl_df(df_tl_geometry, tl1_filter)
+
+
+filt_df           = get_tl_df_all_filters(df_tl_geometry, tl1_filter)
 
 println(filt_df[:,1:7])
 println("N TRANSMISSION LINES:\n", nrow(filt_df))
 
 
-bordering_states = get_bordering_states( df_us_states_info, tl1_filter, 3 )
+bordering_states = get_bordering_states( df_us_states_info, tl1_filter, 1 )
 
 
 for state_i in bordering_states
