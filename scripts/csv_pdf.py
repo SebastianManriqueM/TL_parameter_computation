3024ba3ac29408c268e6b5f08e257c38623f36b8
SebastianManriqueM <prof.sebastian.manrique@gmail.com>
2024-09-12 16:14:25 -0600

    Add ACCC conductor data

--- scripts/csv_pdf.py
+++ scripts/csv_pdf.py
@@ -23,10 +23,24 @@ def remove_text_after_substring(s, substring):
         # If the substring is not found, return the original string
         return s
 
-index_file = 2
-file_name   = ["ACSR_Electrical_Data" , "AAC_Electrical_Data", "ACAR_electrical_data_CME"]
-first_substring = [ "Turkey" , "Peachbell", "Pelican"]
-last_substring  = ["*STOCKED", "*STOCKED", "Kingfisher*"]
+def eliminate_spaces_of_names( original_string, substring ):
+    if substring in original_string:
+        # Find the start and end indices of the substring
+        start_index = original_string.find(substring)
+        end_index = start_index + len(substring)
+        
+        # Extract the substring and replace spaces with underscores
+        modified_substring = original_string[start_index:end_index].replace(" ", "_")
+        
+        # Replace the original substring with the modified substring
+        modified_string = original_string[:start_index] + modified_substring + original_string[end_index:]
+
+    return modified_string
+
+index_file = 3
+file_name   = ["ACSR_Electrical_Data" , "AAC_Electrical_Data", "ACAR_electrical_data_CME", "ACCC_electrical_data_ctc"]
+first_substring = [ "Turkey" , "Peachbell", "Pelican", "OCEANSIDE"]
+last_substring  = ["*STOCKED", "*STOCKED", "Kingfisher*", "SR Bluebird"]
 
 folder_name = "cable_data"
 pdf_path    = folder_name + "/" + file_name[index_file] + ".pdf"
@@ -44,17 +58,31 @@ for page_num in range(len(pdf.pages)):
     print(text)
 
 text = remove_text_before_substring(text, first_substring[index_file])
-text = remove_text_before_substring(text, first_substring[index_file])
+if index_file == 2:
+    text = remove_text_before_substring(text, first_substring[index_file])
 text = remove_text_after_substring( text, last_substring[index_file])
-print(text)
+
+if index_file == 3:
+    names_to_fix = ["CORPUS CHRISTI", "FORT WORTH", "EL PASO", "SAN ANTONIO"]
+    for name in names_to_fix:
+        text = eliminate_spaces_of_names( text, name )
+
+print("\n\n",text)
 
 rows = text.split("\n")
 table = []
+n_col = len( rows[0].split(" ") )
+i=1
 for row in rows:
     columns = row.split(" ")  # assuming space is the column delimiter
     columns = [col.strip() for col in columns if col]  # removing extra spaces
+    
+    while n_col > len(columns):
+        columns.append("ND")
+    print(i, "\t", len(columns))
     if columns:  # ensuring the row is not empty
         table.append(columns)
+    i=i+1
 
         
 
