63800d490bd80df0c8b09c269ab7784ef46c9dfc
SebastianManriqueM <prof.sebastian.manrique@gmail.com>
2024-09-23 13:53:29 -0600

    Fix constant for z calculations in OHMS_MILE

--- src/tl_parameters_calculator.jl
+++ src/tl_parameters_calculator.jl
@@ -16,10 +16,15 @@ function get_primitive_z_matrix(
     
     n_cond      = geometry.n_cables
     z_primitive = zeros( ComplexF64, n_cond, n_cond)
+
+    L_INDTERM = L_INDTERM_OHM_MILE
+    L_CONST   = L_CONST_OHM_MILE
+    R_CONST   = R_CONST_OHM_MILE
+
     #out diagonal
     j = 1
     for i in geometry.combinations
-        z_primitive[i[1] , i[2]] = R_CONST_OHM_MILE*freq + ( L_CONST_OHM_MILE*freq*( log(1/geometry.distances[j]) + L_INDTERM_OHM_MILE + 0.5*log(rho/freq) ) )*im
+        z_primitive[i[1] , i[2]] = R_CONST*freq + ( L_CONST*freq*( log(1/geometry.distances[j]) + L_INDTERM + 0.5*log(rho/freq) ) )*im
         z_primitive[i[2] , i[1]] = z_primitive[i[1] , i[2]]
         j = j + 1
     end
@@ -28,11 +33,11 @@ function get_primitive_z_matrix(
     for i = 1:n_cond
         #TODO Add capability to represent 2 different conductors in different circuits - change Rac_tnom and gmr at elseif
         if i <= 3  #Circuit 1
-            z_primitive[i , i] = conductor.Rac_tnom + R_CONST_OHM_MILE*freq + ( L_CONST_OHM_MILE*freq*( log(1/conductor.gmr)   + L_INDTERM_OHM_MILE + 0.5*log(rho/freq) ) )*im
+            z_primitive[i , i] = conductor.Rac_tnom + R_CONST*freq + ( L_CONST*freq*( log(1/conductor.gmr)   + L_INDTERM + 0.5*log(rho/freq) ) )*im
         elseif  basicdata.n_circuits == 2 && i <= basicdata.n_circuits * 3  #Circuit 2
-            z_primitive[i , i] = conductor.Rac_tnom + R_CONST_OHM_MILE*freq + ( L_CONST_OHM_MILE*freq*( log(1/conductor.gmr)   + L_INDTERM_OHM_MILE + 0.5*log(rho/freq) ) )*im
+            z_primitive[i , i] = conductor.Rac_tnom + R_CONST*freq + ( L_CONST*freq*( log(1/conductor.gmr)   + L_INDTERM + 0.5*log(rho/freq) ) )*im
         else #Ground wire
-            z_primitive[i , i] = ground_wire.Rdc_20 + R_CONST_OHM_MILE*freq + ( L_CONST_OHM_MILE*freq*( log(1/ground_wire.gmr) + L_INDTERM_OHM_MILE + 0.5*log(rho/freq) ) )*im
+            z_primitive[i , i] = ground_wire.Rdc_20 + R_CONST*freq + ( L_CONST*freq*( log(1/ground_wire.gmr) + L_INDTERM + 0.5*log(rho/freq) ) )*im
         end
 
     end
