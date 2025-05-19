a3ef0179e1b42fef85e9b1d78170250798385ca4
Sebastián M <104728742+SebastianManriqueM@users.noreply.github.com>
2025-05-18 19:19:45 -0600

    Change function to other file for code organization

--- src/core/utils_calcs.jl
+++ src/core/utils_calcs.jl
@@ -51,3 +51,50 @@ function get_all_distances(
     end
     return distances, collect(iter)
 end
+
+
+
+#|------------------------------------------------|
+#|-------------GET TL CONDUCTOR STRUCT------------|
+#|________________________________________________|
+
+function get_regpoly_xy_coord( 
+    n_bundling::Int,
+    bundlingspacing::Float64
+    )
+
+    x_coords = zeros(Float64, 1, n_bundling)
+    y_coords = zeros(Float64, 1, n_bundling)
+
+    radius = bundlingspacing / ( 2 * sin( π/n_bundling ) )
+    angle_increment = 2 * π / n_bundling
+
+    for i in 0:( n_bundling - 1 )
+        angle         = i * angle_increment
+        x_coords[i+1] = radius * cos(angle)
+        y_coords[i+1] = radius * sin(angle)
+    end
+
+    return x_coords, y_coords, radius
+end
+
+function get_gmr_bundling_xy( 
+    X1::Matrix{Float64}, 
+    Y1::Matrix{Float64},
+    r::Float64, 
+    gmr_cable::Float64 = r * 1/exp(1/4) #K_gmr = 1/exp(1/4)    # This is for solid/compact conductors. For Stranded see IEC 60287-1-3
+    )
+    
+    GMR_bundle = 1
+    n     = length( X1 )
+    for i = 1 : n
+        for j = 1 : n
+            if i == j 
+                GMR_bundle = GMR_bundle * gmr_cable
+                continue
+            end
+            GMR_bundle = GMR_bundle * get_distance_xy( [ X1[i] X1[j] ], [ Y1[i] Y1[j] ] )#get_distance_p1p2( [ X1[i] Y1[i] ], [ X1[j] Y1[j] ] )
+        end
+    end
+    return GMR_bundle^( 1 / (n * n) )
+end
