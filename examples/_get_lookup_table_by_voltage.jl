9f286a461f363106d87671b5f7f905cae95b267c
Sebastián M <104728742+SebastianManriqueM@users.noreply.github.com>
2025-05-19 19:08:41 -0600

    Add 230 kv Lines and example to get typical line parameters by voltage considering single and double circuit towers

--- examples/_get_lookup_table_by_voltage.jl
+++ examples/_get_lookup_table_by_voltage.jl
@@ -8,9 +8,9 @@ include( joinpath(pwd(), "src/TransmissionLineParameters.jl") )
 df_geometry, df_us_states_, df_cond, df_ground_w, df_tl_ex = load_transmission_line_db( )
 
 #tl_designs_df = CSV.read("examples/tl_designs.csv", DataFrame)
-tl_designs_df = CSV.read("examples/tl_designs_ieee.csv", DataFrame)
+tl_designs_df = CSV.read("examples/tl_designs_ieee_2.csv", DataFrame)
 
-df_parameters = DataFrame(r1_ohm_mile=Float64[], x1_ohm_mile=Float64[], b1_us_mile=Float64[], sil_MW=Float64[])
+df_parameters = DataFrame(n_circuits=Int[], r1_ohm_mile=Float64[], x1_ohm_mile=Float64[], b1_us_mile=Float64[], Ampacity_A=Float64[], MVA = Float64[], sil_MW=Float64[])
 
 for row in eachrow(tl_designs_df)
     @show row["struct_code"]
@@ -43,16 +43,20 @@ for row in eachrow(tl_designs_df)
                                 tl_conductor,
                                 tl_ground_w
                             )
+    ncircuits= tl_basicdata.n_circuits
+    mva = sqrt(3) * tl_basicdata.voltage_kv * tl.conductor.ampacity_bundling / 1000                
     r1 = tl.parameters.r1
     x1 = tl.parameters.x1
     b1 = tl.parameters.b1
+    ampacity = tl.conductor.ampacity_bundling
     sil = tl.parameters.sil
 
+
     # Append the parameters to the DataFrame
-    push!(df_parameters, (r1, x1, b1, sil))
+    push!(df_parameters, (ncircuits, r1, x1, b1, ampacity, mva, sil))
 end
 # Concatenate tl_designs_df and df_parameters horizontally
 df_result = hcat(tl_designs_df, df_parameters)
 @show df_result
 
-CSV.write("examples/tl_designs_with_parameters2.csv", df_result)
+CSV.write("examples/tl_designs_with_parameters_2circuits.csv", df_result)
